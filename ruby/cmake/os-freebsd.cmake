fcd50346f1c151f019debad630e28e68b44d303e
Max Bebök <beboek.max@gmail.com>
2026-09-16 00:24:34 +0200

    fix: more CI issues

--- ruby/cmake/os-freebsd.cmake
+++ ruby/cmake/os-freebsd.cmake
@@ -1,12 +1,6 @@
 target_sources(
   ruby
   PRIVATE #
-    video/sdl3.cpp
-)
-
-target_sources(
-  ruby
-  PRIVATE #
 )
 
 target_sources(
@@ -15,6 +9,5 @@ target_sources(
 )
 
 target_enable_feature(ruby "SDL3 GPU video driver" VIDEO_SDL3)
-target_enable_feature(ruby "SDL3 input driver" INPUT_SDL3)
 
 target_link_libraries(ruby PRIVATE SDL3::SDL3)
